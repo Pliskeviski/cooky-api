c99125452be797d51ab9dd9e36bc6faaf882a63d
Pliskeviski <gustavopliskeviski@gmail.com>
2020-02-29 14:19:55 -0300

    fix(Login): login model

--- Models/BaseModel.cs
+++ Models/BaseModel.cs
@@ -5,8 +5,8 @@ namespace Cooky.API.Models
     public class BaseModel
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
-        public DateTime CreateAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? DeletedAt { get; set; }
     }
 }
